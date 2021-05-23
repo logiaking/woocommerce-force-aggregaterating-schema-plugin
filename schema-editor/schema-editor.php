2efc82d7a39cf2fa1bd1c224cea192f5e6924856
logiaking <38210138+logiaking@users.noreply.github.com>
2021-05-23 22:46:38 +0100

    Create schema-editor plugin

---
+++ schema-editor/schema-editor.php
@@ -0,0 +1,25 @@
+<?php
+/**
+* Plugin Name: Woocommerce Schema Editor
+* Plugin URI: https://github.com/logiaking/woocommerce-overwrite-aggregaterating-schema-plugin
+* Description: A plugin to extend Schema.org data.
+* Version: 1.0
+* Author: LogiaKing
+* Author URI: https://github.com/logiaking
+**/
+
+
+//Change AggregateRating data
+add_filter( 'woocommerce_structured_data_product', 'custom_piece' );
+
+function custom_piece( $data ) {
+	$data['aggregateRating'][] = array (
+	      "@type"=>"AggregateRating",
+	      "ratingValue"=>"5",
+	      "bestRating"=>"5",
+	      "worstRating"=>"1",
+	      "reviewCount"=>"12",
+	      "author"=>"Google"
+	); 
+    return $data;
+}
